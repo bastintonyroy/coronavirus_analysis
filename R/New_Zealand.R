008e24371f507f71c7d3a19eecc76db6ed31860c
REGISTRY\savba95p <bastintonyroy@gmail.com>
2020-09-22 09:34:59 +1200

    Added a new file analysing data for New Zealand

---
+++ R/New_Zealand.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(lubridate)
+data <- read_csv("data/coronavirus_data_2020-09-21.csv")
+
+data %>% 
+  filter(countriesAndTerritories == "New_Zealand") %>% 
+  mutate(Date = dmy(dateRep)) %>% 
+  ggplot(aes(x = Date, y = cases)) +
+  geom_line()
