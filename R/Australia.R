c4e90436d1beb9a7df9e4bbe4acfd4f448e13adf
REGISTRY\savba95p <bastintonyroy@gmail.com>
2020-09-22 07:13:57 +1200

    Added a new visualisation to compare countries and fixed some spacing issues

--- R/Australia.R
+++ R/Australia.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
-data <- read_csv("../data/coronavirus_data_2020-09-21.csv")
+data <- read_csv("data/coronavirus_data_2020-09-21.csv")
+
 data %>% 
   filter(countriesAndTerritories == "Australia") %>% 
   mutate(dateRep = dmy(dateRep)) %>% 
--- R/India.R
+++ R/India.R
@@ -1,8 +1,16 @@
 library(tidyverse)
 library(lubridate)
 data <- read_csv("data/coronavirus_data_2020-09-21.csv")
+
 data %>% 
   filter(countriesAndTerritories == "India") %>% 
   mutate(Date = dmy(dateRep)) %>% 
   ggplot(aes(x = Date, y = cases)) +
   geom_line()
+
+# Comparing number of cases between different countries
+data %>% 
+  filter(countriesAndTerritories %in% c("Australia","India", "Brazil", "United_States_of_America")) %>% 
+  mutate(Date = dmy(dateRep)) %>% 
+  ggplot(aes(x = Date, y = cases)) +
+  geom_line(aes(col = countriesAndTerritories))
